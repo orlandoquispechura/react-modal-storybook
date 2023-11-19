8f96c9e334bac56563cef605a0f4dfd5220eb9c0
orlandoquispechura <softorlando@gmail.com>
2023-11-19 04:03:20 -0400

    subir todo

---
+++ storybook-static/assets/preview-685738b9.js
@@ -0,0 +1,2 @@
+import{_ as e}from"./iframe-9b12b416.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-1b500b7d.js"),["./DocsRenderer-NNNQARDV-1b500b7d.js","./iframe-9b12b416.js","./index-76fb7be0.js","./_commonjsHelpers-de833af9.js","./react-18-063a39db.js","./index-932496f1.js","./index-e1dbc598.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
+//# sourceMappingURL=preview-685738b9.js.map
